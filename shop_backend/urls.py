d4abc8e61bd2507159bf3a09d6119355621c1f42
unknown <erma280191@gmail.com>
2022-01-24 19:45:14 +0600

    beks

--- shop_backend/urls.py
+++ shop_backend/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from shop import views
+from users import views as viewsusers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +9,8 @@ urlpatterns = [
     path('api/v1/products/<int:id>', views.products_detail_view),
     path('api/v1/products/reviews/', views.products_reviews_view),
     path('api/v1/products/tags/', views.products_tags_view),
+
+    path('api/v1/login/', viewsusers.login),
+    path('api/v1/register/', viewsusers.register)
+
 ]
